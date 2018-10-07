fce6437e81840c92faadd97f3af37ac119fd2767
elamathijm <elamathijm@gmail.com>
2018-10-07 11:20:33 +0530

    first commit

---
+++ src/main/java/deltaairlines/DeltaFlight/App.java
@@ -0,0 +1,13 @@
+package deltaairlines.DeltaFlight;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
